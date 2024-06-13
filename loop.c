cc907da9c045ea60ba496ffca0eafee9ad2f227d
Luciano dii Souza <luciano.desouza@workl.com>
2024-06-12 22:05:43 -0300

    feat: while

---
+++ loop.c
@@ -0,0 +1,15 @@
+//
+// Created by Luciano dii Souza on 12/06/24.
+//
+
+#include <stdio.h>
+
+int main() {
+    int counter = 0;
+
+    while(counter < 10) {
+        printf("Hello Dude! %i", counter);
+        counter++;
+    }
+    return 0;
+}
